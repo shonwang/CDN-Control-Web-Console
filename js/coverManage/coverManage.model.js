2a0e2b74e47b72e6c0fdbf29eb2f8f755594a532
Ks3SdkDoc <ks3sdk@kingsoft.com>
2016-01-27 14:59:42 +0800

    map

--- js/coverManage/coverManage.model.js
+++ js/coverManage/coverManage.model.js
@@ -9,6 +9,36 @@ define("coverManage.model", ['require','exports', 'utility'], function(require, 
         model: Model,
 
         initialize: function(){},
+
+        getMapData: function(args){
+            var url = BASE_URL + "/rs/map/data/get";
+            var defaultParas = {
+                type: "GET",
+                url: url,
+                async: true,
+                timeout: 30000,
+            };
+            defaultParas.data = args;
+
+            defaultParas.beforeSend = function(xhr){
+                //xhr.setRequestHeader("Accept","application/json, text/plain, */*");
+            }
+            defaultParas.success = function(res){
+                if (res){
+                    this.trigger("get.map.success", res);
+                } else {
+                    this.trigger("get.map.error", res); 
+                }
+            }.bind(this);
+
+            defaultParas.error = function(response, msg){
+                if (response&&response.responseText)
+                    response = JSON.parse(response.responseText)
+                this.trigger("get.map.error", response); 
+            }.bind(this);
+
+            $.ajax(defaultParas);
+        },
     });
 
     return CoverManageCollection;

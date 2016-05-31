2153bca82b4a14ea1a064cd8f9c863cbb7d950e8
Ks3SdkDoc <ks3sdk@kingsoft.com>
2016-05-31 17:22:21 +0800

    fix bug CDNBUG-372

--- js/businessManage/businessManage.view.js
+++ js/businessManage/businessManage.view.js
@@ -440,7 +440,8 @@ define("businessManage.view", ['require', 'exports', 'template', 'modal.view', '
                 businessType: this.businessType,
                 deviceType: this.deviceType,
                 ipType: this.ipType,
-                nodeCollection: this.nodeCollection
+                nodeCollection: this.nodeCollection,
+                currentBisType: this.Id.business
             });
 
             var options = {

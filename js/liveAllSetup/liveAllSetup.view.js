90082f5f14bb88517061145c7ec1437f63d69ce6
xuyang <xuyang6@kingsoft.com>
2016-06-30 15:48:44 +0800

    update

--- js/liveAllSetup/liveAllSetup.view.js
+++ js/liveAllSetup/liveAllSetup.view.js
@@ -566,7 +566,7 @@ define("liveAllSetup.view", ['require','exports', 'template', 'modal.view', 'uti
             var zNodes = res[this.nodeGroupId];
 
             _.each(zNodes,function(el,index,ls){
-                el.checked = true;
+                //el.checked = true;
                 el.open = false;
                 el.highlight = false;
                 el.nodeIcon = false;
@@ -637,6 +637,7 @@ define("liveAllSetup.view", ['require','exports', 'template', 'modal.view', 'uti
             };
             this.matchDeviceNodes = this.treeObj.getNodesByFilter(matchDeviceFilter);
             this.$el.find(".device-num").html(this.matchDeviceNodes.length);
+
         },
 
         getArgs: function(){
@@ -707,6 +708,7 @@ define("liveAllSetup.view", ['require','exports', 'template', 'modal.view', 'uti
             _.each(nodeGroupIdList, function(itemNodeGroupDevices, key, list){
                 var aNode = "";
                 _.each(this.nodeTreeLists[itemNodeGroupDevices], function(deviceObj, objKey, l){
+                    this.nodeTreeLists[itemNodeGroupDevices][objKey].checked = true;
                     if(deviceObj.pId === -1){
                         aNode += '<li class="node-item" data-deviceId="'+deviceObj.id+'"><span class="label label-primary">'+ deviceObj.name + '</span></li>';
                         var arr = [];
@@ -766,6 +768,7 @@ define("liveAllSetup.view", ['require','exports', 'template', 'modal.view', 'uti
                     if (!resultObj) return;
                     currentNodeDevice.nodes = resultObj.nodes;
                     currentNodeDevice.devices = resultObj.devices;
+                    console.log(this.nodeDeviceArray);
                     nodeCtn.find("li").remove();
                     //if(currentNodeDevice.devices.length > 0){
                         _.each(currentNodeDevice.nodes, function(el, index, ls){
@@ -776,8 +779,9 @@ define("liveAllSetup.view", ['require','exports', 'template', 'modal.view', 'uti
                                 }
                             }.bind(this));
                             var aNode = $('<li class="node-item" data-deviceId="'+deviceIdsList.join(',')+'"><span class="label label-primary">'+ el.name + '</span></li>');
-                            aNode.appendTo(nodeCtn)
+                            aNode.appendTo(nodeCtn);
                         }.bind(this))
+
                         this.addDevicePopup.$el.modal("hide");
                     //}
                 }.bind(this),
@@ -829,7 +833,7 @@ define("liveAllSetup.view", ['require','exports', 'template', 'modal.view', 'uti
                     fileName: file.get("fileName")
                 })
             })
-            // console.log(forCheckList);
+            // console.log(this.nodeDeviceArray);
             // return;
             this.collection.checkLastVersion(forCheckList);
         },

c58c281911c2f2964f7a9f8754cabfc51b99c065
xuyang <xuyang6@kingsoft.com>
2016-07-15 18:51:15 +0800

    update

--- js/ipManage/ipManage.model.js
+++ js/ipManage/ipManage.model.js
@@ -6,6 +6,8 @@ define("ipManage.model", ['require','exports'], function(require, exports) {
             if (status === "2") this.set("statusName", "<span class='text-warning'>暂停中</span>");
             if (status === "4") this.set("statusName", "<span class='text-danger'>宕机</span>");
             if (status === "6") this.set("statusName", "暂停且宕机");
+            if (status === "8")this.set("statusName", "<span class='text-warning'>暂停中</span>");
+            if (status === "10")this.set("statusName", "<span class='text-warning'>暂停中</span>");
         }
     });
 

0439a9e306a90fa116c1944b5862de84ed96147f
Ks3SdkDoc <ks3sdk@kingsoft.com>
2016-10-11 17:24:48 +0800

    联调

--- js/customerSetup/domainList/cacheKeySetup/cacheKeySetup.view.js
+++ js/customerSetup/domainList/cacheKeySetup/cacheKeySetup.view.js
@@ -11,19 +11,66 @@ define("cacheKeySetup.view", ['require','exports', 'template', 'modal.view', 'ut
                 domainInfo = JSON.parse(options.query2),
                 userInfo = {
                     clientName: clientInfo.clientName,
-                    domain: domainInfo.domain
+                    domain: domainInfo.domain,
+                    uid: clientInfo.uid
                 }
+            this.domainInfo = domainInfo;
             this.optHeader = $(_.template(template['tpl/customerSetup/domainList/domainManage.header.html'])({
                 data: userInfo,
                 notShowBtn: true
             }));
             this.optHeader.appendTo(this.$el.find(".opt-ctn"))
 
-            //this.$el.find(".setup-ctn").html(_.template(template['tpl/loading.html'])({}));
+            this.$el.find(".save").on("click", $.proxy(this.onClickSaveButton, this))
 
-            // this.collection.on("get.channel.success", $.proxy(this.onChannelListSuccess, this));
-            // this.collection.on("get.channel.error", $.proxy(this.onGetError, this));
+            this.collection.on("modify.cacheKey.success", $.proxy(this.launchSendPopup, this));
+            this.collection.on("modify.cacheKey.error", $.proxy(this.onGetError, this));
+        },
 
+        launchSendPopup: function(){
+            require(["saveThenSend.view", "saveThenSend.model"], function(SaveThenSendView, SaveThenSendModel){
+                var mySaveThenSendView = new SaveThenSendView({
+                    collection: this.collection, 
+                });
+                var options = {
+                    title: "发布",
+                    body : mySaveThenSendView,
+                    backdrop : 'static',
+                    type     : 2,
+                    onOKCallback:  function(){
+                        this.sendPopup.$el.modal("hide");
+                    }.bind(this),
+                    onHiddenCallback: function(){
+                        if (this.sendPopup) $("#" + this.sendPopup.modalId).remove();
+                    }.bind(this)
+                }
+                this.sendPopup = new Modal(options);
+            }.bind(this))
+        },
+
+        checkDomainName:function(){
+            //检查域名
+            var domainName = this.$el.find("#modify-cache-host").val().trim();
+            if(domainName == ""){
+                alert("不能为空");
+                return false;
+            }
+            var result = Utility.isDomain(domainName);
+            if(!result){
+                alert("填写错误");
+                return false;
+            }
+            return true;
+        },
+
+        onClickSaveButton: function(){
+            var result = this.checkDomainName();
+            if (!result) return
+            var postParam = {
+                originId: this.domainInfo.id,
+                cacheKey: this.$el.find("#modify-cache-host").val().trim(),
+            };
+            this.collection.setCacheKey(postParam);
         },
 
         onGetError: function(error){
@@ -37,12 +84,19 @@ define("cacheKeySetup.view", ['require','exports', 'template', 'modal.view', 'ut
             this.$el.hide();
         },
 
-        update: function(){
-            this.$el.show();
+        update: function(query, query2){
+            this.options.query = query;
+            this.options.query2 = query2;
+            this.collection.off();
+            this.collection.reset();
+            this.$el.remove();
+            this.initialize(this.options);
+            this.render(this.target);
         },
 
-        render: function(target) {
-            this.$el.appendTo(target)
+        render: function(target){
+            this.$el.appendTo(target);
+            this.target = target;
         }
     });
 

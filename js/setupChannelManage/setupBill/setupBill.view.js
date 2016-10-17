b99ec5ef101dd5b711f0dee062829489ff66e7f7
Ks3SdkDoc <ks3sdk@kingsoft.com>
2016-10-17 15:18:56 +0800

    update

--- js/setupChannelManage/setupBill/setupBill.view.js
+++ js/setupChannelManage/setupBill/setupBill.view.js
@@ -35,8 +35,7 @@ define("setupBill.view", ['require','exports', 'template', 'modal.view', 'utilit
             this.allRegion = data;
             this.collection.on("get.version.success",$.proxy(this.onGetVersionInfo,this));
             this.collection.on("get.version.error",$.proxy(this.onGetError,this));
-            //this.collection.getVersion({originId: this.options.originId})
-            this.collection.getVersion({originId: 8901})
+            this.collection.getVersion({originId: this.options.originId})
         },
 
         onGetVersionInfo: function(data) {
@@ -396,29 +395,30 @@ define("setupBill.view", ['require','exports', 'template', 'modal.view', 'utilit
             //     "value": "11" //添加的value
             // }]
             var delTypeName, addTypeName, addDetailsStr = [];
-            if (this.urlParameterVo.delType === 0) delTypeName = "关闭";
-            if (this.urlParameterVo.addType === 0) addTypeName = "关闭";
-            if (this.urlParameterVo.delType === 1) delTypeName = "开启";
-            if (this.urlParameterVo.addType === 1) addTypeName = "开启";
+            if (this.urlParameterVo && this.urlParameterVo.delType === 0) delTypeName = "关闭";
+            if (this.urlParameterVo && this.urlParameterVo.addType === 0) addTypeName = "关闭";
+            if (this.urlParameterVo && this.urlParameterVo.delType === 1) delTypeName = "开启";
+            if (this.urlParameterVo && this.urlParameterVo.addType === 1) addTypeName = "开启";
+            if (this.urlParameterVo) {
+                _.each(this.urlParameterVo.addDetails, function(el, index, ls) {
+                    addDetailsStr.push("参数：" + el.key + ", 值：" + el.value)
+                })
 
-            _.each(this.urlParameterVo.addDetails, function(el, index, ls) {
-                addDetailsStr.push("参数：" + el.key + ", 值：" + el.value)
-            })
+                var list = [{
+                    type:"删除参数",
+                    status: delTypeName,
+                    value: this.urlParameterVo.delKeys
+                }, {
+                    type:"添加参数参数",
+                    status: addTypeName,
+                    value: addDetailsStr.join("<br>")
+                }]
 
-            var list = [{
-                type:"删除参数",
-                status: delTypeName,
-                value: this.urlParameterVo.delKeys
-            }, {
-                type:"添加参数参数",
-                status: addTypeName,
-                value: addDetailsStr.join("<br>")
-            }]
-
-            this.requestArgsModifyTable = $(_.template(template['tpl/setupChannelManage/setupBill/setupBill.requestArgsModify.html'])({
-                data: list
-            }));
-            this.requestArgsModifyTable.appendTo(this.$el.find(".bill-ctn"));
+                this.requestArgsModifyTable = $(_.template(template['tpl/setupChannelManage/setupBill/setupBill.requestArgsModify.html'])({
+                    data: list
+                }));
+                this.requestArgsModifyTable.appendTo(this.$el.find(".bill-ctn"));
+            }
 
             this.initRefererAntiLeech();
         },

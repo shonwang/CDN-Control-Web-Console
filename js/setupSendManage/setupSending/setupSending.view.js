3376a1335e975de236deca8d6c7b69c67938ba29
Ks3SdkDoc <ks3sdk@kingsoft.com>
2016-10-20 16:35:03 +0800

    update

--- js/setupSendManage/setupSending/setupSending.view.js
+++ js/setupSendManage/setupSending/setupSending.view.js
@@ -1,5 +1,32 @@
 define("setupSending.view", ['require','exports', 'template', 'modal.view', 'utility'], function(require, exports, template, Modal, Utility) {
     
+    var ConfiFileDetailView = Backbone.View.extend({
+        events: {
+            //"click .search-btn":"onClickSearch"
+        },
+
+        initialize: function(options) {
+            this.options = options;
+            this.collection = options.collection;
+            this.model      = options.model;
+            this.$el = $(_.template(template['tpl/setupChannelManage/setupChannelManage.editCfgFalse.html'])({
+                data: {},
+                panelId: Utility.randomStr(8)
+            }));
+        },
+
+        onGetError: function(error){
+            if (error&&error.message)
+                alert(error.message)
+            else
+                alert("网络阻塞，请刷新重试！")
+        },
+
+        render: function(target) {
+            this.$el.appendTo(target);
+        }
+    });
+
     var SendDetailView = Backbone.View.extend({
         events: {
             //"click .search-btn":"onClickSearch"
@@ -27,11 +54,46 @@ define("setupSending.view", ['require','exports', 'template', 'modal.view', 'uti
             else
                 this.$el.find(".table-ctn").html(_.template(template['tpl/empty.html'])());
 
-            this.table.find("tbody .view-detail").on("click", $.proxy(this.onClickItemDetail, this));
+            //this.table.find(".node-item").on("click", $.proxy(this.onClickItemDetail, this));
+            this.initDomainList();
+        },
+
+        initDomainList: function(){
+            var data = [{domain: "test1.ksyun.com", id: 1}];
+            this.domainList = $(_.template(template['tpl/setupSendManage/setupSending/setupSending.detail.domain.html'])({
+                data: data, 
+            }));
+            if (data.length !== 0)
+                this.$el.find(".domain-ctn").html(this.domainList[0]);
+            else
+                this.$el.find(".domain-ctn").html(_.template(template['tpl/empty.html'])());
+
+            this.domainList.find(".node-item").on("click", $.proxy(this.onClickItemDetail, this));
         },
 
         onClickItemDetail: function(event){
+            var eventTarget = event.srcElement || event.target,
+                id = $(eventTarget).attr("id");
+            //var model = this.collection.get(id);
 
+            if (this.configFilePopup) $("#" + this.configFilePopup.modalId).remove();
+
+            var myConfiFileDetailView = new ConfiFileDetailView({
+                collection: this.collection, 
+                //model     : model,
+                isEdit    : true
+            });
+            var options = {
+                title: "配置文件详情",//model.get("chName") + "关联调度组信息",
+                body : myConfiFileDetailView,
+                backdrop : 'static',
+                type     : 2,
+                onOKCallback:  function(){
+                    this.configFilePopup.$el.modal("hide");
+                }.bind(this),
+                onHiddenCallback: function(){}.bind(this)
+            }
+            this.configFilePopup = new Modal(options);
         },
 
         onClickCancelButton: function(){

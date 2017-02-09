b81f95d98ebe0e34df664c38689d7f6847562fd3
Ks3SdkDoc <ks3sdk@kingsoft.com>
2017-02-09 15:50:08 +0800

    去重

--- js/customerSetup/domainList/requestArgsModify/requestArgsModify.view.js
+++ js/customerSetup/domainList/requestArgsModify/requestArgsModify.view.js
@@ -81,6 +81,23 @@ define("requestArgsModify.view", ['require','exports', 'template', 'modal.view',
             })
             }.bind(this))
 
+            var isContinue = true;
+
+            _.each(list, function(el, index, ls){
+                if (isContinue) {
+                    var tempObj = _.find(list, function(obj){
+                        return el.parameterKey === obj.parameterKey && el.type === obj.type;
+                    })
+
+                    if (tempObj) {
+                        isContinue = false
+                        alert("添加了重复参数！")
+                    }
+                }
+            }.bind(this)) 
+
+            if (isContinue === false) return;         
+
             var postParam = {
                 "originId": this.domainInfo.id,
                 "list": list

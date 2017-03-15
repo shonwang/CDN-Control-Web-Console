acafc090ba0d4fed0df619a78d75407ef539f169
Ks3SdkDoc <ks3sdk@kingsoft.com>
2017-03-15 14:27:39 +0800

    update

--- js/setupChannelManage/setupChannelManage.view.js
+++ js/setupChannelManage/setupChannelManage.view.js
@@ -483,6 +483,103 @@ define("setupChannelManage.view", ['require', 'exports', 'template', 'modal.view
             }
         });
 
+        var SelectLayerView = Backbone.View.extend({
+            events: {},
+
+            initialize: function(options) {
+                this.options = options;
+                this.collection = options.collection;
+                this.domainArray = options.domainArray;
+
+                this.$el = $(_.template(template['tpl/setupChannelManage/setupChannelManage.select.topo.html'])({ data: { name: "分层策略" } }));
+
+                this.initDomainList();
+
+                this.$el.find("#input-layer").on('keyup', $.proxy(this.onKeyupLayerInput, this));
+
+                require(["setupTopoManage.model"], function(SetupTopoManageModel) {
+                    this.mySetupTopoManageModel = new SetupTopoManageModel();
+                    this.mySetupTopoManageModel.on("get.topoInfo.success", $.proxy(this.initTable, this));
+                    this.mySetupTopoManageModel.on("get.topoInfo.error", $.proxy(this.onGetError, this));
+                    this.mySetupTopoManageModel.getTopoinfo({
+                        name: null,
+                        page: 1,
+                        size: 99999,
+                        type: null
+                    });
+                }.bind(this))
+            },
+
+            initDomainList: function() {
+                this.domainList = $(_.template(template['tpl/setupSendManage/setupSending/setupSending.detail.domain.html'])({
+                    data: this.domainArray,
+                }));
+                if (this.domainArray.length !== 0)
+                    this.$el.find(".domain-ctn").html(this.domainList[0]);
+                else
+                    this.$el.find(".domain-ctn").html(_.template(template['tpl/empty.html'])());
+            },
+
+            onKeyupLayerInput: function() {
+                if (!this.mySetupTopoManageModel.models || this.mySetupTopoManageModel.models.length === 0) return;
+                var keyWord = this.$el.find("#input-layer").val();
+
+                _.each(this.mySetupTopoManageModel.models, function(model, index, list) {
+                    if (keyWord === "") {
+                        model.set("notDisplay", false);
+                    } else {
+                        if (model.get("name").indexOf(keyWord) > -1)
+                            model.set("notDisplay", false);
+                        else
+                            model.set("notDisplay", true);
+                    }
+                }.bind(this));
+                this.initTable();
+            },
+
+            initTable: function() {
+                this.table = $(_.template(template['tpl/specialLayerManage/specialLayerManage.radio.table.html'])({
+                    data: this.mySetupTopoManageModel.models,
+                }));
+                if (this.mySetupTopoManageModel.models.length !== 0)
+                    this.$el.find(".table-ctn").html(this.table[0]);
+                else
+                    this.$el.find(".table-ctn").html(_.template(template['tpl/empty.html'])());
+            },
+
+            onSure: function() {
+                var selectedTopo = this.$el.find("input:checked");
+                if (!selectedTopo.get(0)) {
+                    alert("请选择一个拓扑关系")
+                    return false;
+                }
+                var topoId = selectedTopo.get(0).id,
+                    domainIdArray = [];
+
+                _.each(this.domainArray, function(el, index, ls) {
+                    domainIdArray.push(el.id)
+                }.bind(this))
+
+                var postParam = {
+                    topologyId: topoId,
+                    originIdList: domainIdArray
+                };
+
+                return postParam
+            },
+
+            onGetError: function(error) {
+                if (error && error.message)
+                    alert(error.message)
+                else
+                    alert("网络阻塞，请刷新重试！")
+            },
+
+            render: function(target) {
+                this.$el.appendTo(target);
+            }
+        });
+
         var SelectTopoView = Backbone.View.extend({
             events: {},
 
@@ -576,84 +673,6 @@ define("setupChannelManage.view", ['require', 'exports', 'template', 'modal.view
 
             render: function(target) {
                 this.$el.appendTo(target);
-            }
-        });
-
-        var SelectLayerView = SelectTopoView.extend({
-            events: {},
-
-            initialize: function(options) {
-                this.options = options;
-                this.collection = options.collection;
-                this.domainArray = options.domainArray;
-
-                this.$el = $(_.template(template['tpl/setupChannelManage/setupChannelManage.select.topo.html'])({ data: { name: "分层策略" } }));
-
-                this.initDomainList();
-
-                this.$el.find("#input-layer").on('keyup', $.proxy(this.onKeyupLayerInput, this));
-
-                require(["setupTopoManage.model"], function(SetupTopoManageModel) {
-                    this.mySetupTopoManageModel = new SetupTopoManageModel();
-                    this.mySetupTopoManageModel.on("get.topoInfo.success", $.proxy(this.initTable, this));
-                    this.mySetupTopoManageModel.on("get.topoInfo.error", $.proxy(this.onGetError, this));
-                    this.mySetupTopoManageModel.getTopoinfo({
-                        name: null,
-                        page: 1,
-                        size: 99999,
-                        type: null
-                    });
-                }.bind(this))
-            },
-
-            onKeyupLayerInput: function() {
-
-                console.log("1111")
-                if (!this.mySetupTopoManageModel.models || this.mySetupTopoManageModel.models.length === 0) return;
-                var keyWord = this.$el.find("#input-layer").val();
-
-                _.each(this.mySetupTopoManageModel.models, function(model, index, list) {
-                    if (keyWord === "") {
-                        model.set("notDisplay", false);
-                    } else {
-                        if (model.get("name").indexOf(keyWord) > -1)
-                            model.set("notDisplay", false);
-                        else
-                            model.set("notDisplay", true);
-                    }
-                }.bind(this));
-                this.initTable();
-            },
-
-            initTable: function() {
-                this.table = $(_.template(template['tpl/specialLayerManage/specialLayerManage.radio.table.html'])({
-                    data: this.mySetupTopoManageModel.models,
-                }));
-                if (this.mySetupTopoManageModel.models.length !== 0)
-                    this.$el.find(".table-ctn").html(this.table[0]);
-                else
-                    this.$el.find(".table-ctn").html(_.template(template['tpl/empty.html'])());
-            },
-
-            onSure: function() {
-                var selectedTopo = this.$el.find("input:checked");
-                if (!selectedTopo.get(0)) {
-                    alert("请选择一个拓扑关系")
-                    return false;
-                }
-                var topoId = selectedTopo.get(0).id,
-                    domainIdArray = [];
-
-                _.each(this.domainArray, function(el, index, ls) {
-                    domainIdArray.push(el.id)
-                }.bind(this))
-
-                var postParam = {
-                    topologyId: topoId,
-                    originIdList: domainIdArray
-                };
-
-                return postParam
             }
         });
 

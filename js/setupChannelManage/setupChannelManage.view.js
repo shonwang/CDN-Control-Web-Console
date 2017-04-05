2ffede66f86a107a7203aaa6c5afcd76d8f9dda7
Ks3SdkDoc <ks3sdk@kingsoft.com>
2017-04-05 15:55:27 +0800

    update

--- js/setupChannelManage/setupChannelManage.view.js
+++ js/setupChannelManage/setupChannelManage.view.js
@@ -100,6 +100,8 @@ define("setupChannelManage.view", ['require', 'exports', 'template', 'modal.view
 
                 this.table.find("tbody tr").find("input").on("click", $.proxy(this.onItemCheckedUpdated, this));
                 this.table.find("thead input").on("click", $.proxy(this.onAllCheckedUpdated, this));
+
+                this.table.find(".remark").popover();
             },
 
             onClickMultiModifyTopology: function() {

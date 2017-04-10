981722a650f6769fee7f91aa74a0aa17a9c89bb3
Ks3SdkDoc <ks3sdk@kingsoft.com>
2017-04-10 16:19:58 +0800

    update

--- js/main.js
+++ js/main.js
@@ -113,7 +113,7 @@
   "setupSending.model": "setupSendManage/setupSending/setupSending.model",
   "setupSending.view": "setupSendManage/setupSending/setupSending.view",
   "setupTopoManage.model": "setupTopoManage/setupTopoManage.model",
-  "setupTopoManage.view": "setupTopoManage/setupTopoManage.view1",
+  "setupTopoManage.view": "setupTopoManage/setupTopoManage.view",
   "setupTopoManageSendStrategy.model": "setupTopoManage/setupTopoManageSendStrategy.model",
   "setupTopoManageSendStrategy.view": "setupTopoManage/setupTopoManageSendStrategy.view",
   "statisticsManage.model": "statisticsManage/statisticsManage.model",

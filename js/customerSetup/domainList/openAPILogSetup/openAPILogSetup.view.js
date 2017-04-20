280ece826f34b1e198b70bfc8828442c86be71a1
Ks3SdkDoc <ks3sdk@kingsoft.com>
2017-04-20 16:23:04 +0800

    update

--- js/customerSetup/domainList/openAPILogSetup/openAPILogSetup.view.js
+++ js/customerSetup/domainList/openAPILogSetup/openAPILogSetup.view.js
@@ -1,4 +1,4 @@
-define("openAPILogSetup.view", ['require','exports', 'template', 'modal.view', 'utility'], function(require, exports, template, Modal, Utility) {
+define("openAPILogSetup.view", ['require', 'exports', 'template', 'modal.view', 'utility'], function(require, exports, template, Modal, Utility) {
 
     var OpenAPILogSetupView = Backbone.View.extend({
         events: {},
@@ -7,7 +7,7 @@ define("openAPILogSetup.view", ['require','exports', 'template', 'modal.view', '
             this.collection = options.collection;
             this.options = options;
             this.$el = $(_.template(template['tpl/customerSetup/domainList/openAPILogSetup/openAPILogSetup.html'])());
-            var clientInfo = JSON.parse(options.query), 
+            var clientInfo = JSON.parse(options.query),
                 domainInfo = JSON.parse(options.query2),
                 userInfo = {
                     clientName: clientInfo.clientName,
@@ -27,12 +27,12 @@ define("openAPILogSetup.view", ['require','exports', 'template', 'modal.view', '
             this.defaultParam = {
                 "userId": clientInfo.uid, //Long 用户id
                 "originId": this.domainInfo.id, //String 域名
-                "ks3Id" : null, //Long Ks3日志配置记录id 不返回表示未开通或已关闭
+                "ks3Id": null, //Long Ks3日志配置记录id 不返回表示未开通或已关闭
                 "granularity": 1, //粒度， 1 按天；0按小时; 2 按分钟; 3 30分钟；） 
                 "bucket": "", //String ks3存储位置 
                 "formatId": 1, //Long 日志模板id
-                "formatName" : null, //String 日志模板名称
-                "formatType" : 1, //Integer 日志模板类型
+                "formatName": null, //String 日志模板名称
+                "formatType": 1, //Integer 日志模板类型
                 "fileTemplate": "", //String 日志文件名称模板
                 "pathTemplate": "", //String 日志文件存储地址模板
                 "updateTime": null, //String
@@ -50,52 +50,52 @@ define("openAPILogSetup.view", ['require','exports', 'template', 'modal.view', '
             this.collection.on("get.logSetup.success", $.proxy(this.onGetDomainInfo, this));
             this.collection.on("get.logSetup.eror", $.proxy(this.onGetError, this));
             this.collection.getLogSetup({
-                userId : clientInfo.uid,
-                domainName : this.domainInfo.domain
+                userId: clientInfo.uid,
+                domainName: this.domainInfo.domain
             });
         },
 
-        initDropdown: function(data){
-            var  templateTypeArray = [],
-            rootNode = this.$el.find(".log-template");
+        initDropdown: function(data) {
+            var templateTypeArray = [],
+                rootNode = this.$el.find(".log-template");
 
-            _.each(data.openApiLogFormat, function(el){
+            _.each(data.openApiLogFormat, function(el) {
                 templateTypeArray.push({
                     name: el.name,
                     value: el.id
                 })
             }.bind(this))
 
-            Utility.initDropMenu(rootNode, templateTypeArray, function(value){
+            Utility.initDropMenu(rootNode, templateTypeArray, function(value) {
                 this.defaultParam.formatId = parseInt(value)
             }.bind(this));
 
-            if (!this.defaultParam.formatName){
+            if (!this.defaultParam.formatName) {
                 this.$el.find("#dropdown-log-template .cur-value").html(templateTypeArray[0].name);
                 this.defaultParam.formatId = templateTypeArray[0].value
             } else {
                 this.$el.find("#dropdown-log-template .cur-value").html(this.defaultParam.formatName);
             }
 
-            var  granularityArray = [],
-            rootNode = this.$el.find(".log-granularity");
+            var granularityArray = [],
+                rootNode = this.$el.find(".log-granularity");
 
-            _.each(data.openApiLogGranularity, function(el){
+            _.each(data.openApiLogGranularity, function(el) {
                 granularityArray.push({
                     name: el.name,
                     value: el.granularity
                 })
             }.bind(this))
 
-            Utility.initDropMenu(rootNode, granularityArray, function(value){
+            Utility.initDropMenu(rootNode, granularityArray, function(value) {
                 this.defaultParam.granularity = parseInt(value)
             }.bind(this));
 
-            var defaultValue = _.find(granularityArray, function(object){
+            var defaultValue = _.find(granularityArray, function(object) {
                 return object.value === this.defaultParam.granularityArray;
             }.bind(this));
 
-            if (defaultValue){
+            if (defaultValue) {
                 this.$el.find("#dropdown-log-granularity .cur-value").html(defaultValue.name);
             } else {
                 this.$el.find("#dropdown-log-granularity .cur-value").html(granularityArray[0].name);
@@ -103,8 +103,8 @@ define("openAPILogSetup.view", ['require','exports', 'template', 'modal.view', '
             }
         },
 
-        onGetDomainInfo: function(data){
-            this.defaultParam = _.extend({}, data)        
+        onGetDomainInfo: function(data) {
+            this.defaultParam = _.extend({}, data)
             this.initSetup();
             this.$el.find("#bucket").val(this.defaultParam.bucket);
             this.$el.find("#file-template").val(this.defaultParam.fileTemplate);
@@ -113,12 +113,12 @@ define("openAPILogSetup.view", ['require','exports', 'template', 'modal.view', '
             this.$el.find("#update-date").val(this.defaultParam.updateTime);
 
             this.collection.getAPITemplateInfo({
-                userId : this.clientInfo.uid,
+                userId: this.clientInfo.uid,
                 originId: this.domainInfo.id
             });
         },
 
-        initSetup: function(){
+        initSetup: function() {
             if (!this.defaultParam.ks3Id) {
                 this.$el.find(".ks3-log .togglebutton input").get(0).checked = false;
                 this.$el.find(".log-ctn").hide(200);
@@ -130,33 +130,33 @@ define("openAPILogSetup.view", ['require','exports', 'template', 'modal.view', '
             }
         },
 
-        onSaveSuccess: function(){
+        onSaveSuccess: function() {
             alert("保存成功！")
             this.collection.getLogSetup({
-                userId : this.clientInfo.uid,
-                domainName : this.domainInfo.domain
+                userId: this.clientInfo.uid,
+                domainName: this.domainInfo.domain
             });
         },
 
-        onClickSaveBtn: function(){
+        onClickSaveBtn: function() {
             var postParam = {
                 "userId": this.defaultParam.userId,
-                "originId": this.domainInfo.id, 
-                "action" : this.defaultParam.action,
+                "originId": this.domainInfo.id,
+                "action": this.defaultParam.action,
                 "granularity": this.defaultParam.granularity,
                 "bucket": this.defaultParam.bucket,
                 "formatId": this.defaultParam.formatId,
-                "fileTemplate": this.defaultParam.fileTemplate, 
+                "fileTemplate": this.defaultParam.fileTemplate,
                 "pathTemplate": this.defaultParam.pathTemplate,
                 "interfaceCaller": this.defaultParam.interfaceCaller || $(".user-name").html()
             }
             this.collection.setLogSetup(postParam)
         },
 
-        onClickToggle: function(){
+        onClickToggle: function() {
             var eventTarget = event.srcElement || event.target;
             if (eventTarget.tagName !== "INPUT") return;
-            if (eventTarget.checked){
+            if (eventTarget.checked) {
                 this.defaultParam.action = "start";
                 this.$el.find(".log-ctn").show(200);
             } else {
@@ -165,18 +165,18 @@ define("openAPILogSetup.view", ['require','exports', 'template', 'modal.view', '
             }
         },
 
-        onGetError: function(error){
-            if (error&&error.message)
+        onGetError: function(error) {
+            if (error && error.message)
                 alert(error.message)
             else
                 alert("网络阻塞，请刷新重试！")
         },
 
-        hide: function(){
+        hide: function() {
             this.$el.hide();
         },
 
-        update: function(query, query2, target){
+        update: function(query, query2, target) {
             this.options.query = query;
             this.options.query2 = query2;
             this.collection.off();
@@ -186,7 +186,7 @@ define("openAPILogSetup.view", ['require','exports', 'template', 'modal.view', '
             this.render(target);
         },
 
-        render: function(target){
+        render: function(target) {
             this.$el.appendTo(target);
         }
     });

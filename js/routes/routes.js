73966a0f52eac771a5cf3f8806ed860b22f02449
Ks3SdkDoc <ks3sdk@kingsoft.com>
2017-07-25 17:47:09 +0800

    update

--- js/routes/routes.js
+++ js/routes/routes.js
@@ -27,35 +27,68 @@ define("routes", ['require', 'exports', 'navbar.view',
             },
 
             routes: {
-                "": "customerSetup",
-                "channelManage": "channelManage",
+                // 资源管理
                 "deviceManage/:query": "deviceManage",
                 "nodeManage": "nodeManage",
+                "ipManage": "ipManage",
+                "setupAppManage": "setupAppManage",
+                "setupTopoManage": "setupTopoManage",
+                "specialLayerManage": "specialLayerManage",
+
+                // 调度配置
                 "dispGroup": "dispGroup",
                 "importAssess": "importAssess",
                 "dispConfig": "dispConfig",
                 "importAssess": "importAssess",
-                "specialLayerManage": "specialLayerManage",
                 "importDomainManage": "importDomainManage",
                 "coverRegion": "coverRegion",
                 "coverManage": "coverManage",
-                "liveAllSetup": "liveAllSetup",
-                "liveCurentSetup": "liveCurentSetup",
-                "ipManage": "ipManage",
                 "statisticsManage": "statisticsManage",
-                "refreshManual": "refreshManual",
-                "customMaintenance": "customMaintenance",
                 "domainStatistics": "domainStatistics",
-                "domainManage": "domainManage",
                 "clientStatistics": "clientStatistics",
-                "businessManage": "businessManage",
-                "grayscaleSetup": "grayscaleSetup",
-                "templateManage": "templateManage",
+
+                // 客户配置
+                "": "customerSetup",
                 "customerSetup": "customerSetup",
                 "domainList/:query": "domainList",
                 "blockUrl/:query": "blockUrl",
                 "interfaceQuota/:query": "interfaceQuota",
 
+                //Lua 下载域名配置
+                "domainList/:query/luaBasicInformation/:query2": "basicInformation",
+
+                //直播域名配置
+                "domainList/:query/liveBasicInformation/:query2": "liveBasicInformation",
+                "domainList/:query/liveDomainSetup/:query2": "liveDomainSetup",
+                "domainList/:query/liveCnameSetup/:query2": "liveCnameSetup",
+                "domainList/:query/liveHttpsSetup/:query2": "liveHttpsSetup",
+                "domainList/:query/liveBackOriginSetup/:query2": "liveBackOriginSetup",
+                "domainList/:query/liveBackOriginDetection/:query2": "liveBackOriginDetection",
+                "domainList/:query/liveRefererAntiLeech/:query2": "liveRefererAntiLeech",
+                "domainList/:query/liveTimestamp/:query2": "liveTimestamp",
+                "domainList/:query/liveBusOptimize/:query2": "liveBusOptimize",
+                "domainList/:query/liveH265Setup/:query2": "liveH265Setup",
+                "domainList/:query/liveAudioOnly/:query2": "liveAudioOnly",
+                "domainList/:query/liveEdge302/:query2": "liveEdge302",
+                "domainList/:query/liveHttpFlvOptimize/:query2": "liveHttpFlvOptimize",
+                "domainList/:query/liveRtmpOptimize/:query2": "liveRtmpOptimize",
+                "domainList/:query/liveSLAStatistics/:query2": "liveSLAStatistics",
+                "domainList/:query/liveFrequencyLog/:query2": "liveFrequencyLog",
+                "domainList/:query/openAPILogSetup/:query2": "openAPILogSetup",
+
+                //直播上行域名配置
+                "domainList/:query/liveUpBasicInformation/:query2": "liveUpBasicInformation",
+                "domainList/:query/liveUpBackOriginSetup/:query2": "liveUpBackOriginSetup",
+                "domainList/:query/liveUpFlowNameChange/:query2": "liveUpFlowNameChange",
+
+                //配置下发2.0
+                "setupChannelManage": "setupChannelManage",
+                "setupSendDone": "setupSendDone",
+                "setupSending": "setupSending",
+                "setupSendWaitCustomize": "setupSendWaitCustomize",
+                "setupSendWaitSend": "setupSendWaitSend",
+
+                //Ngnix 下载域名配置(部分废弃)
                 "domainList/:query/basicInformation/:query2": "basicInformation",
                 "domainList/:query/urlBlackList/:query2": "urlBlackList",
                 "domainList/:query/domainSetup/:query2": "domainSetup",
@@ -76,35 +109,18 @@ define("routes", ['require', 'exports', 'navbar.view',
                 "domainList/:query/timestamp/:query2": "timestamp",
                 "domainList/:query/openNgxLog/:query2": "openNgxLog",
 
-                "domainList/:query/liveBasicInformation/:query2": "liveBasicInformation",
-                "domainList/:query/liveDomainSetup/:query2": "liveDomainSetup",
-                "domainList/:query/liveCnameSetup/:query2": "liveCnameSetup",
-                "domainList/:query/liveHttpsSetup/:query2": "liveHttpsSetup",
-                "domainList/:query/liveBackOriginSetup/:query2": "liveBackOriginSetup",
-                "domainList/:query/liveBackOriginDetection/:query2": "liveBackOriginDetection",
-                "domainList/:query/liveRefererAntiLeech/:query2": "liveRefererAntiLeech",
-                "domainList/:query/liveTimestamp/:query2": "liveTimestamp",
-                "domainList/:query/liveBusOptimize/:query2": "liveBusOptimize",
-                "domainList/:query/liveH265Setup/:query2": "liveH265Setup",
-                "domainList/:query/liveAudioOnly/:query2": "liveAudioOnly",
-                "domainList/:query/liveEdge302/:query2": "liveEdge302",
-                "domainList/:query/liveHttpFlvOptimize/:query2": "liveHttpFlvOptimize",
-                "domainList/:query/liveRtmpOptimize/:query2": "liveRtmpOptimize",
-                "domainList/:query/liveSLAStatistics/:query2": "liveSLAStatistics",
-                "domainList/:query/liveFrequencyLog/:query2": "liveFrequencyLog",
-                "domainList/:query/openAPILogSetup/:query2": "openAPILogSetup",
-                //直播上行
-                "domainList/:query/liveUpBasicInformation/:query2": "liveUpBasicInformation",
-                "domainList/:query/liveUpBackOriginSetup/:query2": "liveUpBackOriginSetup",
-                "domainList/:query/liveUpFlowNameChange/:query2": "liveUpFlowNameChange",
+                //配置下发1.0（废弃）
+                "liveAllSetup": "liveAllSetup",
+                "liveCurentSetup": "liveCurentSetup",
+                "domainManage": "domainManage",
+                "businessManage": "businessManage",
+                "grayscaleSetup": "grayscaleSetup",
+                "templateManage": "templateManage",
+                "channelManage": "channelManage",
 
-                "setupChannelManage": "setupChannelManage",
-                "setupAppManage": "setupAppManage",
-                "setupTopoManage": "setupTopoManage",
-                "setupSendDone": "setupSendDone",
-                "setupSending": "setupSending",
-                "setupSendWaitCustomize": "setupSendWaitCustomize",
-                "setupSendWaitSend": "setupSendWaitSend",
+                //张振建提的半途而废的需求（废弃）
+                "refreshManual": "refreshManual",
+                "customMaintenance": "customMaintenance"
             },
 
             execute: function(callback, args) {

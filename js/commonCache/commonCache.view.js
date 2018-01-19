b983967cac5698ef333ecc3d693bdc7db84e3034
zhangwei25 <zhangwei25@kingsoft.com>
2018-01-19 10:34:17 +0800

    添加缓存通用

--- js/commonCache/commonCache.view.js
+++ js/commonCache/commonCache.view.js
@@ -1,9 +1,97 @@
 define("commonCache.view", ['require','exports', 'template', 'modal.view', 'utility'], function(require, exports, template, Modal, Utility) {
+    var AddCacheRule = Backbone.View.extend({
+        events: {},
+        initialize: function(options) {
+            this.options = options;
+            this.collection = options.collection;
+            this.isEdit = options.isEdit;
+            this.model = options.model;
+            this.$el = $(_.template(template['tpl/commonCache/commonCache.cacheRule.add.html'])());
+
+            this.defaultParam = {
+                id:"",
+                codes: "",
+                expireTime: "",
+                locationId:''//编辑时的id
+            }; 
+
+            if (this.isEdit){
+                this.defaultParam.codes = this.model.get("codes") || "";
+                this.defaultParam.expireTime = this.model.get("expireTime") || "";
+                this.defaultParam.id = this.model.get("id");
+            }
+
+            this.initEditTemplate();
+
+        },
+        initEditTemplate:function(){
+            if(this.isEdit){
+                this.$el.find("#args").val(this.defaultParam.codes);
+                this.$el.find("#values").val(this.defaultParam.expireTime);
+            }
+        },
+        onSure: function(){
+
+            var codes = this.$el.find("#args").val(), expireTime = this.$el.find("#values").val();
+            if (codes === "" || expireTime === ""){
+                alert("状态码和缓存时间不能为空");
+                return false
+            } 
+            if(!Utility.isNumber(expireTime)){
+                alert("缓存时间只能填数字");
+                return false;
+            }
+
+
+            var postParam = {
+                "id": this.isEdit ? this.model.get("id") : new Date().valueOf(),
+                "locationId": this.defaultParam.locationId,
+                "codes": codes,
+                "expireTime": parseInt(expireTime)
+            }
+            return postParam
+        },
+
+        render: function(target) {
+            this.$el.appendTo(target);
+        }
+    });    
+
     var CacheView = Backbone.View.extend({
         initialize:function(options){
             this.collection = options.collection;
             this.$el = $(_.template(template['tpl/commonCache/commonCache.cacheRule.html'])());
             this.setCacheRule();
+            this.$el.find(".create").on("click", $.proxy(this.onClickAddCacheRule, this));
+        },
+
+
+        onClickAddCacheRule: function(event){
+            if (this.addCacheRulePopup) $("#" + this.addCacheRulePopup.modalId).remove();
+            var myAddEditCacheRuleView = new AddCacheRule({
+                collection: this.collection,
+                isEdit:false
+            });
+
+            var options = {
+                title:"添加缓存规则",
+                body : myAddEditCacheRuleView,
+                backdrop : 'static',
+                type     : 2,
+                onOKCallback: function(){
+                    var postParam = myAddEditCacheRuleView.onSure();
+                    if (!postParam) return;
+                    var args = {
+                        "originId":this.domainInfo.id,
+                        "codes":  postParam.codes,
+                        "expireTime": postParam.expireTime
+                    };
+                    //this.collection.addStatusCode(args);
+                    this.addCacheRulePopup.$el.modal('hide');
+                }.bind(this),
+                onHiddenCallback: function(){}.bind(this)
+            }
+            this.addCacheRulePopup = new Modal(options);
         },
 
         setCacheRule:function(){
@@ -18,6 +106,224 @@ define("commonCache.view", ['require','exports', 'template', 'modal.view', 'util
         render:function(target){
             this.$el.appendTo(target);
         }
+    });
+
+    var AddWhiteView = Backbone.View.extend({
+        events: {},
+        initialize: function(options) {
+            this.options = options;
+            this.collection = options.collection;
+            this.isEdit = options.isEdit;
+            this.model = options.model;
+            this.$el = $(_.template(template['tpl/commonCache/commonCache.white.add.html'])());
+
+            this.defaultParam = {
+                id:"",
+                codes: "",
+                expireTime: "",
+                locationId:''//编辑时的id
+            }; 
+
+            if (this.isEdit){
+                this.defaultParam.codes = this.model.get("codes") || "";
+                this.defaultParam.expireTime = this.model.get("expireTime") || "";
+                this.defaultParam.id = this.model.get("id");
+            }
+
+            this.initEditTemplate();
+
+        },
+        initEditTemplate:function(){
+            if(this.isEdit){
+                this.$el.find("#args").val(this.defaultParam.codes);
+                this.$el.find("#values").val(this.defaultParam.expireTime);
+            }
+        },
+        onSure: function(){
+
+            var codes = this.$el.find("#args").val(), expireTime = this.$el.find("#values").val();
+            if (codes === "" || expireTime === ""){
+                alert("状态码和缓存时间不能为空");
+                return false
+            } 
+            if(!Utility.isNumber(expireTime)){
+                alert("缓存时间只能填数字");
+                return false;
+            }
+
+
+            var postParam = {
+                "id": this.isEdit ? this.model.get("id") : new Date().valueOf(),
+                "locationId": this.defaultParam.locationId,
+                "codes": codes,
+                "expireTime": parseInt(expireTime)
+            }
+            return postParam
+        },
+
+        render: function(target) {
+            this.$el.appendTo(target);
+        }
+    });  
+
+    var IpBlackWhiteView = Backbone.View.extend({
+        initialize:function(options){
+            this.collection = options.collection;
+            this.$el = $(_.template(template['tpl/commonCache/commonCache.ipBlackWhite.html'])());
+            this.setCacheRule();
+            this.$el.find(".create").on("click", $.proxy(this.onClickAddCacheRule, this));
+        },
+
+
+        onClickAddCacheRule: function(event){
+            if (this.addAddWhitePopup) $("#" + this.addAddWhitePopup.modalId).remove();
+            var myAddWhiteView = new AddWhiteView({
+                collection: this.collection,
+                isEdit:false
+            });
+
+            var options = {
+                title:"添加白名单",
+                body : myAddWhiteView,
+                backdrop : 'static',
+                type     : 2,
+                onOKCallback: function(){
+                    var postParam = myAddWhiteView.onSure();
+                    if (!postParam) return;
+                    var args = {
+                        "originId":this.domainInfo.id,
+                        "codes":  postParam.codes,
+                        "expireTime": postParam.expireTime
+                    };
+                    //this.collection.addStatusCode(args);
+                    this.addAddWhitePopup.$el.modal('hide');
+                }.bind(this),
+                onHiddenCallback: function(){}.bind(this)
+            }
+            this.addAddWhitePopup = new Modal(options);
+        },
+
+        setCacheRule:function(){
+            this.table = $(_.template(template['tpl/commonCache/commonCache.ipBlackWhite.table.html'])());
+            this.$el.find(".table-ctn").html(this.table);
+        },
+
+        updateView:function(){
+
+        },
+
+        render:function(target){
+            this.$el.appendTo(target);
+        }        
+    });
+
+    var AddClearCacheView = Backbone.View.extend({
+        events: {},
+        initialize: function(options) {
+            this.options = options;
+            this.collection = options.collection;
+            this.isEdit = options.isEdit;
+            this.model = options.model;
+            this.$el = $(_.template(template['tpl/commonCache/commonCache.clearCache.add.html'])());
+
+            this.defaultParam = {
+                id:"",
+                codes: "",
+                expireTime: "",
+                locationId:''//编辑时的id
+            }; 
+
+            if (this.isEdit){
+                this.defaultParam.codes = this.model.get("codes") || "";
+                this.defaultParam.expireTime = this.model.get("expireTime") || "";
+                this.defaultParam.id = this.model.get("id");
+            }
+
+            this.initEditTemplate();
+
+        },
+        initEditTemplate:function(){
+            if(this.isEdit){
+                this.$el.find("#args").val(this.defaultParam.codes);
+                this.$el.find("#values").val(this.defaultParam.expireTime);
+            }
+        },
+        onSure: function(){
+
+            var codes = this.$el.find("#args").val(), expireTime = this.$el.find("#values").val();
+            if (codes === "" || expireTime === ""){
+                alert("状态码和缓存时间不能为空");
+                return false
+            } 
+            if(!Utility.isNumber(expireTime)){
+                alert("缓存时间只能填数字");
+                return false;
+            }
+
+
+            var postParam = {
+                "id": this.isEdit ? this.model.get("id") : new Date().valueOf(),
+                "locationId": this.defaultParam.locationId,
+                "codes": codes,
+                "expireTime": parseInt(expireTime)
+            }
+            return postParam
+        },
+
+        render: function(target) {
+            this.$el.appendTo(target);
+        }
+    });  
+
+    var ClearCacheView = Backbone.View.extend({
+        initialize:function(options){
+            this.collection = options.collection;
+            this.$el = $(_.template(template['tpl/commonCache/commonCache.clearCache.html'])());
+            this.setCacheRule();
+            this.$el.find(".create").on("click", $.proxy(this.onClickAddCacheRule, this));
+        },
+
+
+        onClickAddCacheRule: function(event){
+            if (this.addClearCachePopup) $("#" + this.addClearCachePopup.modalId).remove();
+            var myAddClearCacheView = new AddClearCacheView({
+                collection: this.collection,
+                isEdit:false
+            });
+
+            var options = {
+                title:"添加白名单",
+                body : myAddClearCacheView,
+                backdrop : 'static',
+                type     : 2,
+                onOKCallback: function(){
+                    var postParam = myAddWhiteView.onSure();
+                    if (!postParam) return;
+                    var args = {
+                        "originId":this.domainInfo.id,
+                        "codes":  postParam.codes,
+                        "expireTime": postParam.expireTime
+                    };
+                    //this.collection.addStatusCode(args);
+                    this.addClearCachePopup.$el.modal('hide');
+                }.bind(this),
+                onHiddenCallback: function(){}.bind(this)
+            }
+            this.addClearCachePopup = new Modal(options);
+        },
+
+        setCacheRule:function(){
+            this.table = $(_.template(template['tpl/commonCache/commonCache.clearCache.table.html'])());
+            this.$el.find(".table-ctn").html(this.table);
+        },
+
+        updateView:function(){
+
+        },
+
+        render:function(target){
+            this.$el.appendTo(target);
+        }        
     });
 
     var CommonCacheView = Backbone.View.extend({
@@ -45,6 +351,39 @@ define("commonCache.view", ['require','exports', 'template', 'modal.view', 'util
              }
             //this.onClickQueryButton();
             this.setCacheView();//一上来首先加载列表
+            this.$el.find('a[data-toggle="tab"]').on('shown.bs.tab', $.proxy(this.onShownTab, this));
+        },
+
+        onShownTab: function (e) {
+            var eventTarget = e.target; // newly activated tab
+            var id = $(eventTarget).attr("data-target");
+            relatedID = $(e.relatedTarget).attr("data-target");
+            switch(id){
+                case "#valuable-cache-rule":
+                    this.currentTab = "#valuable-cache-rule";
+                    
+                break;
+                case "#valuable-cache-ipBlackAndWhite":
+                    this.currentTab = "#valuable-cache-ipBlackAndWhite";
+                    if(this.ipBlackWhiteView) {
+                        return;
+                    }
+                    this.ipBlackWhiteView = new IpBlackWhiteView({
+                        collection: this.liveCollection
+                    })
+                    this.ipBlackWhiteView.render(this.$el.find("#valuable-cache-ipBlackAndWhite"))
+                break;
+                case "#valuable-cache-clearRule":
+                    this.currentTab = "#valuable-cache-clearRule";
+                    if(this.clearCacheView) {
+                        return;
+                    }
+                    this.clearCacheView = new ClearCacheView({
+                        collection: this.liveCollection
+                    })
+                    this.clearCacheView.render(this.$el.find("#valuable-cache-clearRule"))
+                break;
+            }
         },
 
         setCacheView:function(){

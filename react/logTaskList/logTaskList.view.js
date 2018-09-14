9ef0807d3f760c8151442f5cad7205488746b7c2
wangzhisong <wangzhisong@kingsoft.com>
2018-09-14 15:06:08 +0800

    update

--- react/logTaskList/logTaskList.view.js
+++ react/logTaskList/logTaskList.view.js
@@ -156,7 +156,7 @@ define("logTaskList.view", ['require','exports', 'template', 'base.view', 'utili
                     onOk: function(){
                         var ltProps = this.props.ltProps;
                         var collection = ltProps.collection;
-                        collection.stopTask({groupId: id});
+                        collection.stopTask({id: id});
                     }.bind(this)
                   });
             }
